602dbf909cd86478b6664389a6a61c5ae1430317
interesting1113 <sylviayang1113@gmail.com>
2023-04-20 19:06:56 +0800

    upload

--- src/shared/globals/helpers/cloudinary-uploads.ts
+++ src/shared/globals/helpers/cloudinary-uploads.ts
@@ -4,5 +4,20 @@ export function uploads(
 	file: string,
 	public_id?: string,
 	overwrite?: boolean,
-	ivalidate?: boolean
-): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {}
+	invalidate?: boolean
+): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {
+	return new Promise((resolve) => {
+		cloudinary.v2.uploader.upload(
+			file,
+			{
+				public_id,
+				overwrite,
+				invalidate
+			},
+			(error: UploadApiErrorResponse | undefined, result: UploadApiResponse | undefined) => {
+				if (error) resolve(error);
+				resolve(result);
+			}
+		);
+	});
+}
